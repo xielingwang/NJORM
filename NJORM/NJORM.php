330d0334872a9b6c2c0fb885697f9903b330b673
Amin by <xielingwang@gmail.com>
2014-12-22 11:14:18 +0800

    NJORM, NJQuery

--- NJORM/NJORM.php
+++ NJORM/NJORM.php
@@ -3,5 +3,11 @@
  * @Author: Amin by
  * @Date:   2014-12-15 10:22:32
  * @Last Modified by:   Amin by
- * @Last Modified time: 2014-12-17 18:15:40
+ * @Last Modified time: 2014-12-19 11:45:37
  */
+
+class NJORM {
+  function __get($table) {
+    return (new NJQuery())->from($table);
+  }
+}
--- NJORM/NJQuery.php
+++ NJORM/NJQuery.php
@@ -3,5 +3,27 @@
  * @Author: Amin by
  * @Date:   2014-12-16 17:29:22
  * @Last Modified by:   Amin by
- * @Last Modified time: 2014-12-17 18:15:37
+ * @Last Modified time: 2014-12-19 11:49:45
  */
+
+class NJQuery {
+  function from($table) {
+
+  }
+
+  function select() {
+
+  }
+
+  function where() {
+
+  }
+
+  function and() {
+
+  }
+
+  function or() {
+    
+  }
+}
