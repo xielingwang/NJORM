d3e342f1ae0ae3373205f39bad76bc8f7870e26b
Amin by <xielingwang@gmail.com>
2014-12-25 21:39:24 +0800

    go

--- tests/NSOrderTest.php
+++ tests/NSOrderTest.php
@@ -2,12 +2,16 @@
 /**
  * @Author: byamin
  * @Date:   2014-12-25 01:06:58
- * @Last Modified by:   byamin
- * @Last Modified time: 2014-12-25 01:10:05
+ * @Last Modified by:   Amin by
+ * @Last Modified time: 2014-12-25 21:38:03
  */
 use \NJORM\NJOrder as NJOrd;
 class NJWhereTest extends PHPUnit_Framework_TestCase {
   function testOrder() {
     $o = new NJOrd("field");
+    $this->assertEquals('`field`', $o->toString());
+
+    $o->add("field2", false);
+    $this->assertEquals('`field`, `field2` DESC', $o->toString());
   }
 }
