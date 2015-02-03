09dd8d94798579113df2681a00dd0a19c40198f2
Amin By <xielingwang@gmail.com>
2015-02-04 01:04:16 +0800

    njtable

--- NJORM/NJTable.php
+++ NJORM/NJTable.php
@@ -3,20 +3,27 @@
  * @Author: byamin
  * @Date:   2015-02-02 23:27:30
  * @Last Modified by:   byamin
- * @Last Modified time: 2015-02-03 00:33:30
+ * @Last Modified time: 2015-02-04 01:00:58
  */
 
 namespace NJORM;
 
 class NJTable {
+  protected $_name;
   protected $_primary;
+  // field => alias
   protected $_fields;
 
-  public function __construct($alias) {
-    $this->_alias = $alias;
+  public function __construct($name) {
+    $this->_name = $name;
+  }
+
+  public function __get($name) {
+    return $this->columns($name, $this->_alias);
   }
 
   public function columns($cols='*', $ta=null, $da=null) {
+    $ta === true && $ta = $this->_name;
     if(is_string($cols)) {
       $cols = explode(',', $cols);
     }
@@ -30,7 +37,7 @@ class NJTable {
         $col = sprintf("`%s` `%s`", $this->_fields[$col], $col);
       }
       elseif(in_array($col, $this->_fields)) {
-        $col = sprintf("`%s` `%s`", $col, array_search($col, $this->_fields));
+        $col = sprintf("`%s` `%s`", array_search($col, $this->_fields), $col);
       }
 
       if($ta) {
@@ -45,10 +52,29 @@ class NJTable {
     return implode(',', $newcols);
   }
 
+  public function field($field, $alias) {
+    if(!$this->_fields)
+      $this->_fields = array();
+    $this->_fields[$field] = $alias;
+    return $this;
+  }
+
+  public function primary($field, $alias) {
+    $this->field($field, $alias);
+
+    if(!$this->_primary)
+      $this->_primary = array();
+    array_unshift($this->_primary, $field);
+    return $this;
+  }
+
   protected static $_tables = array();
   static function define($name, $alias) {
-    if(!array_key_exists($name, static::$_tables))
-      static::$_tables[$name] = new static($alias);
-    return static::$_tables[$name];
+    if(!array_key_exists($alias, static::$_tables))
+      static::$_tables[$alias] = new static($name);
+    else
+      trigger_error('table alias exists ' . $alias);
+
+    return static::$_tables[$alias];
   }
 }
--- tests/NJTableTest.php
+++ tests/NJTableTest.php
@@ -3,13 +3,25 @@
  * @Author: byamin
  * @Date:   2015-02-02 23:46:24
  * @Last Modified by:   byamin
- * @Last Modified time: 2015-02-03 00:28:49
+ * @Last Modified time: 2015-02-04 01:03:46
  */
-use \NJORM\NJCom\NJTable;
+use \NJORM\NJTable;
 
 class NJTableTest extends PHPUnit_Framework_TestCase {
-  public function testLimit() {
-    $arr = array('a' => 'c', 'b' => 'e', 'd' => 's');
-    echo array_search('e', $arr);
+  public function testColumns() {
+    $table = NJTable::define('user', 'tu')
+    ->primary('ID', 'id')
+    ->field('username', 'un')
+    ->field('password', 'pwd')
+    ->field('last_login', 'll')
+    ->field('create_at', 'ca');
+
+    $string = $table->columns('id,un,pwd,ll');
+    $this->assertEquals('`ID` `id`,`username` `un`,`password` `pwd`,`last_login` `ll`', $string);
+
+    $string = $table->columns('id,un,pwd,ll', 'tu');
+    $this->assertEquals('`tu`.`ID` `id`,`tu`.`username` `un`,`tu`.`password` `pwd`,`tu`.`last_login` `ll`', $string);
+    return $table;
   }
+
 }
